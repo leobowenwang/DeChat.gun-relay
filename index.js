939fc9849e8997b5f07edc6ad06df6c911e24877
Leo Bowen Wang <leobwang@gmail.com>
2022-03-19 17:30:45 +0100

    change port

--- index.js
+++ index.js
@@ -2,10 +2,9 @@
 const express = require("express");
 const GUN = require("gun");
 const app = express();
-const port = 3030;
 app.use(GUN.serve);
 
-const server = app.listen(process.env.port || port, () => {
+const server = app.listen(process.env.port, () => {
   console.log(`Example app listening at http://${process.env.PUBLIC_URL}:${process.env.port}`);
 });
 
